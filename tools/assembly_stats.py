c182ae004987c9451a4e57b7854fd37ffb0f50bf
Andrew Wilson <andrew.wilson849@icloud.com>
2019-07-08 13:35:12 -0700

    Removed GC skew calculations from assembly_stats.py

--- tools/assembly_stats.py
+++ tools/assembly_stats.py
@@ -12,40 +12,18 @@ import math
 
 # o = open(snakemake.output[0], "w")
 GC_content = {}
-GC_skew = {}
 cov = {}
 length = {}
 seq_len = 0
 sequence = {}
 
-contigs = []
-with open("data/contigs.fasta", "r") as c:
-    line = c.readline()
-    while line:
-        if line.startswith('>'):
-            contigs.append(line.replace('>', '').rstrip())
-            line = c.readline()
-        else:
-            seq_len += len(line.rstrip())
-            line = c.readline()
-
-for c in contigs:
-    info = c.split('_')
+contigs = {}
+for seq in SeqIO.parse(open("data/CH261/assembly/contigs.fasta", "r"), 'fasta'):
+    info = seq.id.split("_")
     contigname = f"{info[0]}_{info[1]}"
-    length[contigname] = info[3]
-    cov[contigname] = info[5]
-    if length[contigname] > 1000:
+    
 
 
 
-print(seq_len)
+#def is_phix()
 
-
-def gc_skew(seq:str, k=5):
-    skew = {}
-    for i in range(0, len(seq) - k):
-        kmer = seq[i:i+k]
-        gua = kmer.count("G")
-        cyt = kmer.count("C")
-        skew[i] = (gua - cyt) / (gua + cyt)
-    return skew
